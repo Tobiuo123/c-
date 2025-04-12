9d6ebf9cb239b88379fea5d3c1796df586501414
Tobiuo <y2164685762@Gmail.com>
2025-04-12 17:14:15 +0800

    Add files via upload
    在Studio code运行这个总是乱码，改成英文也是，没有代码也是一样回复，太奇怪了，换成了community试试

---
+++ change.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    // int price = 0;
+    // printf("Please enter the amount:");
+    // scanf("%d", &price);
+    // // int change = 100 - price;
+    // // printf("Your change is %d yuan.\n", price);
+    printf("Hello world!\n");
+    return 0;
+}
